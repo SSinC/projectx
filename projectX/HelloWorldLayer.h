95856aff6708715e1b0b6d2ad1c18a19611ce5b6
wktzjz <wktzjz@gmail.com>
2013-04-15 17:29:41 +0800

    Many changes

--- projectX/HelloWorldLayer.h
+++ projectX/HelloWorldLayer.h
@@ -16,6 +16,13 @@
 #import "CCPhysicsSprite.h"
 #import "contactListener.h"
 
+//added to support slice and draw Polygon-body
+#import "PolygonSprite.h"
+#import "RaycastCallback.h"
+
+#define calculate_determinant_2x2(x1,y1,x2,y2) x1*y2-y1*x2
+#define calculate_determinant_2x3(x1,y1,x2,y2,x3,y3) x1*y2+x2*y3+x3*y1-y1*x2-y2*x3-y3*x1
+
 //Pixel to metres ratio. Box2D uses metres as the unit for measurement.
 //This ratio defines how many pixels correspond to 1 Box2D "metre"
 //Box2D is optimized for objects of 1x1 metre therefore it makes sense
@@ -56,27 +63,43 @@ typedef struct userdata{
     int32 tagBodyA;
     int32 tagBodyB;
     
-    //add C++ style Array 
-    std::vector<b2Body*> explodingBodiesCPP_;
-    std::vector<b2Vec2> enterPointsVecCPP_;
-    std::vector<b2Body*> enterPointsVecBodyCPP_;
-    std::vector<b2Body*> slicedBodiesCPP_;
+    //add rayCastCallBack
+    RaycastCallback *_raycastCallback;
+
     
-    std::vector<b2Body*>::iterator pos1_;
-    std::vector<b2Vec2>::iterator pos2_;
-    std::vector<b2Body*>::iterator pos3_;
-    std::vector<b2Body*>::iterator pos4_;
+    //add C++ style Array 
+//    std::vector<b2Body*> explodingBodiesCPP_;
+//    std::vector<b2Vec2> enterPointsVecCPP_;
+//    std::vector<b2Body*> enterPointsVecBodyCPP_;
+//    std::vector<b2Body*> slicedBodiesCPP_;
+//    
+//    std::vector<b2Body*>::iterator pos1_;
+//    std::vector<b2Vec2>::iterator pos2_;
+//    std::vector<b2Body*>::iterator pos3_;
+//    std::vector<b2Body*>::iterator pos4_;
     
 }
 
 
-@property(nonatomic) std::vector<b2Body*> explodingBodiesCPP;
-@property(nonatomic) std::vector<b2Vec2> enterPointsVecCPP;
-@property(nonatomic) std::vector<b2Body*> ::iterator pos1;
-@property(nonatomic) std::vector<b2Vec2> ::iterator pos2;
+//@property(nonatomic) std::vector<b2Body*> explodingBodiesCPP;
+//@property(nonatomic) std::vector<b2Vec2> enterPointsVecCPP;
+//@property(nonatomic) std::vector<b2Body*> ::iterator pos1;
+//@property(nonatomic) std::vector<b2Vec2> ::iterator pos2;
 // returns a CCScene that contains the HelloWorldLayer as the only child
 +(CCScene *) scene;
 
 +(HelloWorldLayer*)shareInstance;
+
+
+/***********************************************************
+ createBody: 此方法创建了活跃的可以和其他body产生碰撞的Box2D body。
+ arrangeVertices: 此方法按照逆时针的顺序重排顶点。它使用qsort方法按x坐标升序排列，然后使用determinants来完成最终的重排。
+ comparator: 此方法被qsort使用，它完成顶点比较并返回结果给qsort。
+ areVerticesAcceptable: 目前，此方法假设所有的顶点都是合理的。
+ ********************************************************/
+-(b2Vec2*)arrangeVertices:(b2Vec2*)vertices count:(int)count;
+-(void)splitPolygonSprite:(PolygonSprite*)sprite;
+-(BOOL)areVerticesAcceptable:(b2Vec2*)vertices count:(int)count;
+-(b2Body*)createBodyWithPosition:(b2Vec2)position rotation:(float)rotation vertices:(b2Vec2*)vertices vertexCount:(int32)count density:(float)density friction:(float)friction restitution:(float)restitution;
 
 @end
--- projectX/HelloWorldLayer.mm
+++ projectX/HelloWorldLayer.mm
@@ -39,13 +39,25 @@ enum {
 @end
 
 
+int comparator(const void *a, const void *b) {
+    const b2Vec2 *va = (const b2Vec2 *)a;
+    const b2Vec2 *vb = (const b2Vec2 *)b;
+    
+    if (va->x > vb->x) {
+        return 1;
+    } else if (va->x < vb->x) {
+        return -1;
+    }
+    return 0;
+}
+
 
 HelloWorldLayer* instance;
 @implementation HelloWorldLayer
-@synthesize enterPointsVecCPP = enterPointsVecCPP_;
-@synthesize explodingBodiesCPP = explodingBodiesCPP_;
-@synthesize pos1 = pos1_;
-@synthesize pos2 = pos2_;
+//@synthesize enterPointsVecCPP = enterPointsVecCPP_;
+//@synthesize explodingBodiesCPP = explodingBodiesCPP_;
+//@synthesize pos1 = pos1_;
+//@synthesize pos2 = pos2_;
 
 
 
@@ -113,7 +125,7 @@ HelloWorldLayer* instance;
 #endif
 		
 		CCLOG(@"parent %@",parent);
-        //在我的mac机上下面这句有问题，要改成parentNode--wk
+       
         //[self addChild:parent z:0 tag:kTagParentNode];
 		[self addChild:parentNode z:0 tag:kTagParentNode];
 
@@ -130,6 +142,9 @@ HelloWorldLayer* instance;
         //add contactListener
           _contactListener = new contactListener();
 		 world->SetContactListener(_contactListener);
+        
+        //add rayCastCallBack
+        _raycastCallback = new RaycastCallback();
         
         //Loop-update
 		[self scheduleUpdate];
@@ -454,48 +469,48 @@ HelloWorldLayer* instance;
     selSprite = nil;
 }
 
-class RayCastAnyCallback : public b2RayCastCallback
-{
-public:
-    RayCastAnyCallback()
-    {
-        m_hit = false;
-    }
-    
-    float32 ReportFixture(    b2Fixture* fixture, const b2Vec2& point,
-                          const b2Vec2& normal, float32 fraction)
-    {
-        //这里要完成的功能如下：
-        b2Body* body=fixture->GetBody();
-        
-        //[HelloWorldLayer shareInstance].explodingBodiesCPP;
-        [HelloWorldLayer shareInstance].pos1 = std::find([HelloWorldLayer shareInstance].explodingBodiesCPP.begin(), [HelloWorldLayer shareInstance].explodingBodiesCPP.end(), body);
-        
-        if ( [HelloWorldLayer shareInstance].pos1 ==[HelloWorldLayer shareInstance].explodingBodiesCPP.end() ) {
-            return 1;
-        }
-        
-          
-        [HelloWorldLayer shareInstance].pos2 = std::find([HelloWorldLayer shareInstance].enterPointsVecCPP.begin(), [HelloWorldLayer shareInstance].enterPointsVecCPP.end(), body);
-        
-        if ( [HelloWorldLayer shareInstance].pos2 ==[HelloWorldLayer shareInstance].enterPointsVecCPP.end() ) {
-            [HelloWorldLayer shareInstance].enterPointsVecCPP.push_back(point);
-        }
-        else
-        {
-            [[HelloWorldLayer shareInstance] splitObj:fixture->GetBody() A:*[HelloWorldLayer shareInstance].pos2 B: point];
-        }
-        return 1;
-        
-           }
-    bool m_hit;
-    b2Vec2 m_point;
-    b2Vec2 m_normal;
-};
+//class RayCastAnyCallback : public b2RayCastCallback
+//{
+//public:
+//    RayCastAnyCallback()
+//    {
+//        m_hit = false;
+//    }
+//    
+//    float32 ReportFixture(    b2Fixture* fixture, const b2Vec2& point,
+//                          const b2Vec2& normal, float32 fraction)
+//    {
+//        //这里要完成的功能如下：
+//        b2Body* body=fixture->GetBody();
+//        
+//        //[HelloWorldLayer shareInstance].explodingBodiesCPP;
+//        [HelloWorldLayer shareInstance].pos1 = std::find([HelloWorldLayer shareInstance].explodingBodiesCPP.begin(), [HelloWorldLayer shareInstance].explodingBodiesCPP.end(), body);
+//        
+//        if ( [HelloWorldLayer shareInstance].pos1 ==[HelloWorldLayer shareInstance].explodingBodiesCPP.end() ) {
+//            return 1;
+//        }
+//        
+//        
+//        [HelloWorldLayer shareInstance].pos2 = std::find([HelloWorldLayer shareInstance].enterPointsVecCPP.begin(), [HelloWorldLayer shareInstance].enterPointsVecCPP.end(), body);
+//        
+//        if ( [HelloWorldLayer shareInstance].pos2 ==[HelloWorldLayer shareInstance].enterPointsVecCPP.end() ) {
+//            [HelloWorldLayer shareInstance].enterPointsVecCPP.push_back(point);
+//        }
+//        else
+//        {
+//            [[HelloWorldLayer shareInstance] splitObj:fixture->GetBody() A:*[HelloWorldLayer shareInstance].pos2 B: point];
+//        }
+//        return 1;
+//        
+//           }
+//    bool m_hit;
+//    b2Vec2 m_point;
+//    b2Vec2 m_normal;
+//};
 
 
 -(void) update: (ccTime) dt
-{
+{   
 	//It is recommended that a fixed time step is used with Box2D for stability
 	//of the simulation, however, we are using a variable time step here.
 	//You need to make an informed choice, the following URL is useful
@@ -610,6 +625,291 @@ public:
 
 
 ///////////////////////一些用于计算分裂效果的基础函数
+
+
+-(void)splitPolygonSprite:(PolygonSprite*)sprite
+{
+    //declare & initialize variables to be used for later
+    PolygonSprite *newSprite1, *newSprite2;
+    
+    //our original shape's attributes
+    b2Fixture *originalFixture = sprite.body->GetFixtureList();
+    b2PolygonShape *originalPolygon = (b2PolygonShape*)originalFixture->GetShape();
+    int vertexCount = originalPolygon->GetVertexCount();
+    
+    for (int i = 0 ; i < vertexCount; i++)
+    {
+        b2Vec2 point = originalPolygon->GetVertex(i);
+    }
+    
+    //our determinant(to be described later) and iterator
+    float determinant;
+    int i;
+    
+    //we store the vertices of our two new sprites here
+    b2Vec2 *sprite1Vertices = (b2Vec2*)calloc(24, sizeof(b2Vec2));
+    b2Vec2 *sprite2Vertices = (b2Vec2*)calloc(24, sizeof(b2Vec2));
+    b2Vec2 *sprite1VerticesSorted, *sprite2VerticesSorted;
+    
+    //we store how many vertices there are for each of the two new sprites here
+    int sprite1VertexCount = 0;
+    int sprite2VertexCount = 0;
+    
+    //step 1:
+    //the entry and exit point of our cut are considered vertices of our two new shapes, so we add these before anything else
+    sprite1Vertices[sprite1VertexCount++] = sprite.entryPoint;
+    sprite1Vertices[sprite1VertexCount++] = sprite.exitPoint;
+    sprite2Vertices[sprite2VertexCount++] = sprite.entryPoint;
+    sprite2Vertices[sprite2VertexCount++] = sprite.exitPoint;
+    
+    //step 2:
+    //iterate through all the vertices and add them to each sprite's shape
+    for (i=0; i<vertexCount; i++)
+    {
+        //get our vertex from the polygon
+        b2Vec2 point = originalPolygon->GetVertex(i);
+        
+        //we check if our point is not the same as our entry or exit point first
+        b2Vec2 diffFromEntryPoint = point - sprite.entryPoint;
+        b2Vec2 diffFromExitPoint = point - sprite.exitPoint;
+        
+        if ((diffFromEntryPoint.x == 0 && diffFromEntryPoint.y == 0) || (diffFromExitPoint.x == 0 && diffFromExitPoint.y == 0))
+        {
+        }
+        else
+        {
+            determinant = calculate_determinant_2x3(sprite.entryPoint.x, sprite.entryPoint.y, sprite.exitPoint.x, sprite.exitPoint.y, point.x, point.y);
+            
+            if (determinant > 0)
+            {
+                //if the determinant is positive, then the three points are in clockwise order
+                sprite1Vertices[sprite1VertexCount++] = point;
+            }
+            else
+            {
+                //if the determinant is 0, the points are on the same line. if the determinant is negative, then they are in counter-clockwise order
+                sprite2Vertices[sprite2VertexCount++] = point;
+                
+            }//endif
+        }//endif
+    }//endfor
+    
+    //step 3:
+    //Box2D needs vertices to be arranged in counter-clockwise order so we reorder our points using a custom function
+    sprite1VerticesSorted = [self arrangeVertices:sprite1Vertices count:sprite1VertexCount];
+    sprite2VerticesSorted = [self arrangeVertices:sprite2Vertices count:sprite2VertexCount];
+    
+    //step 4:
+    //Box2D has some restrictions with defining shapes, so we have to consider these. We only cut the shape if both shapes pass certain requirements from our function
+    BOOL sprite1VerticesAcceptable = [self areVerticesAcceptable:sprite1VerticesSorted count:sprite1VertexCount];
+    BOOL sprite2VerticesAcceptable = [self areVerticesAcceptable:sprite2VerticesSorted count:sprite2VertexCount];
+    
+    //step 5:
+    //we destroy the old shape and create the new shapes and sprites
+    if (sprite1VerticesAcceptable && sprite2VerticesAcceptable)
+    {
+        //create the first sprite's body
+        b2Body *body1 = [self createBodyWithPosition:sprite.body->GetPosition() rotation:sprite.body->GetAngle() vertices:sprite1VerticesSorted vertexCount:sprite1VertexCount density:originalFixture->GetDensity() friction:originalFixture->GetFriction() restitution:originalFixture->GetRestitution()];
+        
+        //create the first sprite
+        
+        newSprite1 = [PolygonSprite spriteWithTexture:sprite.texture body:body1 original:NO];
+        [self addChild:newSprite1 z:1];
+        
+        //create the second sprite's body
+        b2Body *body2 = [self createBodyWithPosition:sprite.body->GetPosition() rotation:sprite.body->GetAngle() vertices:sprite2VerticesSorted vertexCount:sprite2VertexCount density:originalFixture->GetDensity() friction:originalFixture->GetFriction() restitution:originalFixture->GetRestitution()];
+        
+        //create the second sprite
+        newSprite2 = [PolygonSprite spriteWithTexture:sprite.texture body:body2 original:NO];
+        [self addChild:newSprite2 z:1];
+        
+        //we don't need the old shape & sprite anymore so we either destroy it or squirrel it away
+        if (sprite.original)
+        {
+            [sprite deactivateCollisions];
+            sprite.position = ccp(-256,-256);   //cast them faraway
+            sprite.sliceEntered = NO;
+            sprite.sliceExited = NO;
+            sprite.entryPoint.SetZero();
+            sprite.exitPoint.SetZero();
+        }
+        else
+        {
+            world->DestroyBody(sprite.body);
+            [self removeChild:sprite cleanup:YES];
+        }
+    }
+    else
+    {
+        sprite.sliceEntered = NO;
+        sprite.sliceExited = NO;
+    }
+    
+    //free up our allocated vectors
+    free(sprite1VerticesSorted);
+    free(sprite2VerticesSorted);
+    free(sprite1Vertices);
+    free(sprite2Vertices);
+}
+
+
+-(b2Body*)createBodyWithPosition:(b2Vec2)position rotation:(float)rotation vertices:(b2Vec2*)vertices vertexCount:(int32)count density:(float)density friction:(float)friction restitution:(float)restitution
+{
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position = position;
+    bodyDef.angle = rotation;
+    b2Body *body = world->CreateBody(&bodyDef);
+    
+    b2FixtureDef fixtureDef;
+    fixtureDef.density = density;
+    fixtureDef.friction = friction;
+    fixtureDef.restitution = restitution;
+    
+    b2PolygonShape shape;
+    shape.Set(vertices, count);
+    fixtureDef.shape = &shape;
+    body->CreateFixture(&fixtureDef);
+    
+    return body;
+}
+
+-(b2Vec2*)arrangeVertices:(b2Vec2*)vertices count:(int)count
+{
+    float determinant;
+    int iCounterClockWise = 1;
+    int iClockWise = count - 1;
+    int i;
+    
+    b2Vec2 referencePointA,referencePointB;
+    b2Vec2 *sortedVertices = (b2Vec2*)calloc(count, sizeof(b2Vec2));
+    
+    //sort all vertices in ascending order according to their x-coordinate so we can get two points of a line
+    qsort(vertices, count, sizeof(b2Vec2), comparator);
+    
+    sortedVertices[0] = vertices[0];
+    referencePointA = vertices[0];          //leftmost point
+    referencePointB = vertices[count-1];    //rightmost point
+    
+    //we arrange the points by filling our vertices in both clockwise and counter-clockwise directions using the determinant function
+    for (i=1;i<count-1;i++)
+    {
+        determinant = calculate_determinant_2x3(referencePointA.x, referencePointA.y, referencePointB.x, referencePointB.y, vertices[i].x, vertices[i].y);
+        if (determinant<0)
+        {
+            sortedVertices[iCounterClockWise++] = vertices[i];
+        }
+        else
+        {
+            sortedVertices[iClockWise--] = vertices[i];
+        }//endif
+    }//endif
+    
+    sortedVertices[iCounterClockWise] = vertices[count-1];
+    return sortedVertices;
+}
+
+-(BOOL)areVerticesAcceptable:(b2Vec2*)vertices count:(int)count
+{
+    //check 1: polygons need to at least have 3 vertices
+    if (count < 3)
+    {
+        return NO;
+    }
+    
+    //check 2: the number of vertices cannot exceed b2_maxPolygonVertices
+    if (count > b2_maxPolygonVertices)
+    {
+        return NO;
+    }
+    
+    //check 3: Box2D needs the distance from each vertex to be greater than b2_epsilon
+    int32 i;
+    for (i=0; i<count; ++i)
+    {
+        int32 i1 = i;
+        int32 i2 = i + 1 < count ? i + 1 : 0;
+        b2Vec2 edge = vertices[i2] - vertices[i1];
+        if (edge.LengthSquared() <= b2_epsilon * b2_epsilon)
+        {
+            return NO;
+        }
+    }
+    
+    //check 4: Box2D needs the area of a polygon to be greater than b2_epsilon
+    float32 area = 0.0f;
+    
+    b2Vec2 pRef(0.0f,0.0f);
+    
+    for (i=0; i<count; ++i)
+    {
+        b2Vec2 p1 = pRef;
+        b2Vec2 p2 = vertices[i];
+        b2Vec2 p3 = i + 1 < count ? vertices[i+1] : vertices[0];
+        
+        b2Vec2 e1 = p2 - p1;
+        b2Vec2 e2 = p3 - p1;
+        
+        float32 D = b2Cross(e1, e2);
+        
+        float32 triangleArea = 0.5f * D;
+        area += triangleArea;
+    }
+    
+    if (area <= 0.0001)
+    {
+        return NO;
+    }
+    
+    //check 5: Box2D requires that the shape be Convex.
+    float determinant;
+    float referenceDeterminant;
+    b2Vec2 v1 = vertices[0] - vertices[count-1];
+    b2Vec2 v2 = vertices[1] - vertices[0];
+    referenceDeterminant = calculate_determinant_2x2(v1.x, v1.y, v2.x, v2.y);
+    
+    for (i=1; i<count-1; i++)
+    {
+        v1 = v2;
+        v2 = vertices[i+1] - vertices[i];
+        determinant = calculate_determinant_2x2(v1.x, v1.y, v2.x, v2.y);
+        //we use the determinant to check direction from one point to another. A convex shape's points should only go around in one direction. The sign of the determinant determines that direction. If the sign of the determinant changes mid-way, then we have a concave shape.
+        if (referenceDeterminant * determinant < 0.0f)
+        {
+            //if multiplying two determinants result to a negative value, we know that the sign of both numbers differ, hence it is concave
+            return NO;
+        }
+    }
+    v1 = v2;
+    v2 = vertices[0]-vertices[count-1];
+    determinant = calculate_determinant_2x2(v1.x, v1.y, v2.x, v2.y);
+    if (referenceDeterminant * determinant < 0.0f)
+    {
+        return NO;
+    }
+    return YES;
+}
+
+
+-(void)checkAndSliceObjects
+{
+    double curTime = CACurrentMediaTime();
+    for (b2Body* b = world->GetBodyList(); b; b = b->GetNext())
+    {
+        if (b->GetUserData() != NULL) {
+            PolygonSprite *sprite = (PolygonSprite*)b->GetUserData();
+            
+            if (sprite.sliceEntered && curTime > sprite.sliceEntryTime)
+            {
+                sprite.sliceEntered = NO;
+            }
+            else if (sprite.sliceEntered && sprite.sliceExited)
+            {
+                [self splitPolygonSprite:sprite];
+            }
+        }
+    }
+}
 
 // - (float) intersection:(b2Fixture *)fixture point:(b2Vec2 *)point normal:(b2Vec2 *)normal fraction:(float)fraction
 //{
